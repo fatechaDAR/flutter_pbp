3c7545b66e9af775f2e83668418d9386f95d643b
FatechaDena <fatechaanggarahmatulloh@gmail.com>
2025-09-15 19:40:33 +0700

    splash_page

---
+++ lib/splash_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    // Delay 3 detik, lalu pindah ke LoginPage
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const LoginPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: CircleAvatar(
+          radius: 80,
+          backgroundColor: Colors.transparent,
+          child: Image.asset('assets/pudidi.png'),
+        ),
+      ),
+    );
+  }
+}
