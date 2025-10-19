adc7ce15c33886479540b03faa5949528da50cf5
fatechaDAR <fatechaDAR@users.noreply.github.com>
2025-10-19 12:40:58 +0000

    deploy: b049c0ddf1841447b7e9f2ba2cbc298253d6f94b

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2159819647"
+    serviceWorkerVersion: "422437500"
   }
 });
