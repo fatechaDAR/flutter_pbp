ebb119a981e7a2d585331dab0e7299f52c1a5e86
Techa <fatechaanggarahmatulloh@gmail.com>
2025-10-19 11:54:03 +0000

    splash_nama

--- lib/splash_page.dart
+++ lib/splash_page.dart
@@ -12,7 +12,7 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    // Delay 3 detik, lalu pindah ke LoginPage
+    
     Future.delayed(const Duration(seconds: 6), () {
       Navigator.pushReplacement(
         context,
@@ -31,14 +31,28 @@ class _SplashPageState extends State<SplashPage> {
         children: [
 
           Center(
-            child: CircleAvatar(
-              radius: 180,
-              backgroundColor: Colors.transparent,
-              child: Image.asset('assets/logo.png'),
+            child: Column( 
+              mainAxisAlignment: MainAxisAlignment.center, 
+              children: [
+                CircleAvatar(
+                  radius: 180,
+                  backgroundColor: Colors.transparent,
+                  child: Image.asset('assets/logo.png'),
+                ),
+                const SizedBox(height: 20), 
+                const Text( // 
+                  'PAGE & PLAY', 
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.indigo, 
+                  ),
+                ),
+              ],
             ),
           ),
 
-          
+        
           Align(
             alignment: Alignment.bottomLeft,
             child: Padding(
